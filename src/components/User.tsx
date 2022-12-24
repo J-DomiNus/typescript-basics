b043b7a234fe2d1105540a48207bcf2eaa28a0fc
Joel Dominguez <joeru.desu@gmail.com>
2022-12-24 09:41:12 -0300

    first commit

---
+++ src/components/User.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface User {
+  uid: string;
+  name: string;
+}
+
+const tempUser: User = {
+  uid: "ASD789",
+  name: "Jorgito Martinez",
+};
+
+const User = () => {
+  const [user, setUser] = useState<User>(tempUser);
+  const login = () => {
+    setUser({
+      uid: "ABC123",
+      name: "Juan Perez",
+    });
+  };
+  return (
+    <div className="mt-5">
+      <h3>Usuario: useState</h3>
+      <button onClick={login} className="btn btn-outline-primary">
+        Login
+      </button>
+      {!user ? <pre>No user</pre> : <pre>{JSON.stringify(user)}</pre>}
+    </div>
+  );
+};
+
+export default User;
